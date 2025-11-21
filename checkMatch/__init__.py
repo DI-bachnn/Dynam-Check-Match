87a0fb62b5f968bdc95872e2d2ad802f6a46a0c8
Nguyen Ngoc Bach <nguyen.ngoc.bach@dataimpact.vn>
2025-11-21 12:52:09 +0700

    test: Update name

--- checkMatch/__init__.py
+++ checkMatch/__init__.py
@@ -20,7 +20,7 @@ def read_csv_with_jp_encoding(blob_client):
 # ==============================
 # Hàm chính HTTP trigger
 # ==============================
-def checkMatch(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+def checkMatchData(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     output_lines = []
     logger = context.logger
 
